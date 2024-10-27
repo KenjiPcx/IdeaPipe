c20c45beccdcb0abf6452f7bcc9ba060a4107f30
Kenji Phang <ken.pcx@outlook.com>
2024-10-27 17:41:35 +0000

    setup directories

--- archive/run_app.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Run backend
-start cmd /k run_backend.bat
-
-:: Wait a bit for the backend to start
-timeout /t 5
-
-:: Run frontend
-start cmd /k run_frontend.bat
--- archive/run_backend.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Activate virtual environment
-call venv\Scripts\activate
-
-:: Install dependencies
-pip install -r requirements.txt
-
-:: Run the FastAPI server
-uvicorn app.main:app --reload
